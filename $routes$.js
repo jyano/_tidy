f24431e545c8ddb4b5542948903c133de185ef8a
yano <jason.yanofski@gmail.com>
2016-01-19 09:38:51 -0800

    tmp ..organized.. lets see what works.. now routes set, too. not just dirs

--- $routes$.js
+++ $routes$.js
@@ -1,63 +1,47 @@
- 
-Stage = ['cjs', 'cjsMC',
-	'createCore', 'cjsEvents', 'stage', 'ticker', 'loader', 'tran',
-	'objects', 'matrix', 'cjsPoly',
-	'cjsGx', 'cjsShape', 'curve', 'domElOb', 'cjsGrad',
-	'reggy', 'transform', 'cjsText', 'container',
-	'cjsCir', 'cjsRec', 'official', 'interesting', 'cache', 'filter',
-	'cjsColor',
-	'alphaMaskFilter', 'alphaMapFilter', 'blurFilter', 'graphTest',
-	'cache',
-	'colorFilter', 'colorMatrixFilter', 'bitmap', 'cjsMouse',
-	'tween', 'easing', 'sprite', 'ssBuilder', 'zoe', 'SSData',
-	'movie', 'timeline', 'gunnertron', 'layers', 'tranTemp'
-]
-
-
-
-Graphics = [
-	'gpc', 'pureDa',
-	'jqGraphics', 'gpClipper', 'superCtx', 'superCan',
-	'ctxDraw', 'ctxImg', 'ctxGlobComp', 'ctxGrad', 'ctxMug', 'ctxPixels', 'ctxShad',
-	'ctxText', 'ctxTransform',
-	'canDraw', 'canImg', 'canGlobComp', 'canMug', 'canPixels', 'canShad',
-	'canText', 'canTransform',
-	'gpcCtx',
-	'parallax'
-]
-Front = [
-	'mock',
-	'jqui', 'uiWids', 'custWids', 'widLS', 'effects', 'interactions', 'dog', 'wid', 'widLS',
-	'bb', 'bone', 'bbColl', 'bbDirt', 'bbEvt', 'bbFiltColl', 'bbRtr', 'bbView', 'bbLS', 'bbServer', 'bbServerMore',
-	'ko', 'knock', 'koCustom', 'koMore'
-]
-
-Wap = ['wappy', 'users', 'login', 'social', 'widUser']
-
-
-
-Web = ['both', 'jq','web','img','l', 'superCanvas','context'
-]
-
-
-
-Box = [
-
-	'bx', 'boxBase', 'boxBool', 
-	'boxMath', 'boxCan',
-	'fixtPt','fixDefPt','bodyPt',
-	'bDefPt','boxShape',
-	'worldPrt', 'worldQuery','joints',
-	'mouseJt',
-	'debug', 'loop', 'superBox',
-	'boxApps','forcesApps', 'worldGravity',
-	'makeWorld',
-	'bxWebsiteEx1',
-	'bxWebsiteEx2',
-	'bxMouseApps','collApps',
-	'mouseJtApps','watchKeysApps'
-]
-
+Anim = ['anim','tween','wapSprite', 'meSprite', 'shake']
+CtxCore = ['context', 'ctxCan', 'ctxDataURL', 'ctxSaveRestore', 'ctxWH', 'getCtx', 'makeCtx']
+Ctx = [
+//draw
+		'ctxColor', 'ctxCurve', 'ctxPattern', 'ctxPol', 'ctxRec',
+//drawImg
+		'ctxCenter', 'ctxCrop', 'ctxDrawImg', 'ctxFit',
+//globComp
+		'ctxGlobComp',
+		//grad
+		'ctxGradProto', 'ctxLinGrad', 'ctxRadGrad',
+//imgDa
+		'ctxImgData', 'x_jD', 'ctxLineProps', 'ctxMtLt',
+//path
+		'ctxClip', 'ctxPath', 'ctxPtInPth',
+		//shadow
+		'ctxShad',
+		//text
+		'ctxFont', 'ctxText',
+		//transform
+		'ctxTf', 'ctxTranslate', 'ctxScale', 'ctxRotate'
+	]
+CtxApps = [
+//drawImg
+		'ctxDrawImgApps', 'ctxADB',
+//glob comp
+		'ctxGlobCompApps',
+//grad
+		'ctxGradApps',
+//imgDa
+		'ctxPixelApps',
+//loader
+		'ctxLdrApps',
+//draw
+		'ctxDrawApps', 'ctxPathApps',
+//shadow
+		'ctxShadApps',
+//text
+		'ctxTextApps',
+//tf
+		'clipStars', 'ctxRtApps', 'ctxTfApps'
+	]
+Can = ['canText', 'canText', 'canPixel', 'canGrad', 'canApps']
+Clipper = ['clipper', 'clipperCanvas', 'operations', 'polyDefault', 'geoBool']
 Easel = ['cjs','createBase','cjsEvents', 'cjsMouse', 'dobs', 'official','stage',
 
 'bitmap',
@@ -70,78 +54,48 @@ Easel = ['cjs','createBase','cjsEvents', 'cjsMouse', 'dobs', 'official','stage',
 	'cirApps','circles','official','circle',
 	'filter','alphaMaskFilter','blurFlApps',
 	'cjsGradApps','radialApps','shapeGrad'
-	
+		
+]
+Fiz = [
+	'bx', 'boxBase', 'boxBool',
+	'boxMath', 'boxCan',
+	'fixtPt', 'fixDefPt', 'bodyPt',
+	'bDefPt', 'boxShape',
+	'worldPrt', 'worldQuery', 'joints',
+	'mouseJt',
+	'debug', 'loop', 'superBox',
+	'boxApps', 'forcesApps', 'worldGravity',
+	'makeWorld',
+	'bxWebsiteEx1',
+	'bxWebsiteEx2',
+	'bxMouseApps', 'collApps',
+	'mouseJtApps', 'watchKeysApps'
+]
+Front = [
+	'mock',
+	'jqui', 'uiWids', 'custWids', 'widLS', 'effects', 'interactions', 'dog', 'wid', 'widLS',
+	'bb', 'bone', 'bbColl', 'bbDirt', 'bbEvt', 'bbFiltColl', 'bbRtr', 'bbView', 'bbLS', 'bbServer', 'bbServerMore',
+	'ko', 'knock', 'koCustom', 'koMore'
+]
+Wapp = ['wapInit','cutouts', 'upload','uploads','sockStart',
+
+'ct_mug','fn_fitMug','getMug',
+
+'users', 'login','edit','fancyEditUsesDragFrame',
+	'loginBar', 'social', 'widUser'
 
 ]
-
-
-CtxCore = ['context','ctxCan','ctxDataURL','ctxSaveRestore','ctxWH','getCtx','makeCtx']
-Ctx = [
-
-//draw
-	'ctxColor','ctxCurve','ctxPattern','ctxPol','ctxRec'	,
-//drawImg
-	'ctxCenter','ctxCrop','ctxDrawImg','ctxFit',
-//globComp
-	'ctxGlobComp',
-	//grad
-	'ctxGradProto','ctxLinGrad','ctxRadGrad',
-//imgDa
-	'ctxImgData','x_jD','ctxLineProps','ctxMtLt',
-
-//path
-	'ctxClip', 'ctxPath','ctxPtInPth',
-	
-	//shadow
-	'ctxShad',
-	
-	//text
-	'ctxFont','ctxText',
-	
-	//transform
-	'ctxTf','ctxTranslate','ctxScale','ctxRotate'
-	
-	
-	
-]
-
-CtxApps = [
-
-//drawImg
-'ctxDrawImgApps','ctxADB',
-//glob comp
-'ctxGlobCompApps',
-//grad
-'ctxGradApps',
-//imgDa
-'ctxPixelApps',
-
-//loader
-'ctxLdrApps',
-
-//draw
-	'ctxDrawApps','ctxPathApps',
-
-//shadow
-'ctxShadApps',
-
-//text
-'ctxTextApps',
-
-//tf
-'clipStars','ctxRtApps','ctxTfApps'
-]
-
-Can=['canText', 'canText', 'canPixel', 'canGrad', 'canApps']
-
+Webb = ['both', 'jq', 'web', 'events', 'css', 'html', 'img', 'l']
 jsArrs= [
-  Web,
-Can,
-  CtxCore, Ctx,CtxApps,
-  Easel,
-  Box,//, Front,
-   Wap
+	Webb,
+	Front,
+	Can, CtxCore, Ctx,CtxApps, 
+	Clipper, 
+	Easel,  Anim,
+	Wapp,
+	Fiz
 ]
+
 
 
 $a.get('/box/:app*', function (q, p) {
@@ -157,4 +111,4 @@ $a.get('/box/:app*', function (q, p) {
 
 $l('no theme')
 //html += '<link rel="stylesheet" type="text/css"  href="/theme.css"></script>'
-	
+ 
